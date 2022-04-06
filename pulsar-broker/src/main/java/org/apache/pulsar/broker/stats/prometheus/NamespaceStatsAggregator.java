81927aa279ad2d9ac2bf732d7ea814411e8ebbff
Christophe Bornet <cbornet@hotmail.com>
2022-04-06 15:37:59 +0200

    Fix subscription consumer count
    (cherry picked from commit 1d4f9a684894b7d6e96895e1db554910bba0d115)

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/stats/prometheus/NamespaceStatsAggregator.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/stats/prometheus/NamespaceStatsAggregator.java
@@ -32,6 +32,7 @@ import org.apache.pulsar.broker.service.persistent.PersistentTopic;
 import org.apache.pulsar.common.policies.data.stats.ConsumerStatsImpl;
 import org.apache.pulsar.common.policies.data.stats.NonPersistentTopicStatsImpl;
 import org.apache.pulsar.common.policies.data.stats.ReplicatorStatsImpl;
+import org.apache.pulsar.common.policies.data.stats.SubscriptionStatsImpl;
 import org.apache.pulsar.common.policies.data.stats.TopicStatsImpl;
 import org.apache.pulsar.common.util.SimpleTextOutputStream;
 import org.apache.pulsar.compaction.CompactedTopicContext;
@@ -189,6 +190,7 @@ public class NamespaceStatsAggregator {
                 subsStats.lastConsumedFlowTimestamp = subscriptionStats.lastConsumedFlowTimestamp;
                 subsStats.lastConsumedTimestamp = subscriptionStats.lastConsumedTimestamp;
                 subsStats.lastMarkDeleteAdvancedTimestamp = subscriptionStats.lastMarkDeleteAdvancedTimestamp;
+                subsStats.consumersCount = subscriptionStats.consumers.size();
                 subscriptionStats.consumers.forEach(cStats -> {
                     stats.consumersCount++;
                     subsStats.unackedMessages += cStats.unackedMessages;
@@ -222,6 +224,7 @@ public class NamespaceStatsAggregator {
                 subsStats.lastConsumedFlowTimestamp = subscriptionStats.getLastConsumedFlowTimestamp();
                 subsStats.lastConsumedTimestamp = subscriptionStats.getLastConsumedTimestamp();
                 subsStats.lastMarkDeleteAdvancedTimestamp = subscriptionStats.getLastMarkDeleteAdvancedTimestamp();
+                subsStats.consumersCount = subscriptionStats.getConsumers().size();
                 subsStats.msgDropRate = subscriptionStats.getMsgDropRate();
                 subscriptionStats.getConsumers().forEach(cStats -> {
                     stats.consumersCount++;
